415d3fdc7329609f6ed240efcf9bf208dfb87c9e
Aryat Srivastava <107941072+aryat10@users.noreply.github.com>
2024-08-24 23:47:15 +0530

    Time: 211 ms (9.80%), Space: 21 MB (6.94%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int func(int ind, vector<int>& nums, vector<int>& dp) {
+        if (ind >= nums.size() - 1) 
+            return 0;  // If we're at or past the last index, no more jumps are needed
+
+        if (dp[ind] != -1) 
+            return dp[ind];  // If already computed, return the stored result
+
+        int mini = INT_MAX;
+        for (int i = 1; i <= nums[ind]; i++) {
+            int jumps = func(ind + i, nums, dp);
+            if (jumps != INT_MAX) {
+                mini = min(mini, jumps + 1);  
+            }
+        }
+
+        dp[ind] = mini;  
+        return mini;
+    }
+
+    int jump(vector<int>& nums) {
+        vector<int> dp(nums.size(), -1);  
+        return func(0, nums, dp);
+    }
+};
