9622b044d37160c339e0f449189fa8ead72ca7c6
Aryat Srivastava <107941072+aryat10@users.noreply.github.com>
2024-11-28 19:40:17 +0530

    Update 0904-fruit-into-baskets.cpp

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
-         unordered_map<int, int> fruitCount; // Map to store the count of each fruit type
-    int left = 0; // Left boundary of the sliding window
-    int maxFruits = 0; // Variable to track the maximum number of fruits collected
+         unordered_map<int, int> fruitCount; 
+    int left = 0; 
+    int maxFruits = 0; 
 
     for (int right = 0; right < fruits.size(); right++) {
-        fruitCount[fruits[right]]++; // Add the current fruit to the map
+        fruitCount[fruits[right]]++; 
 
-        // If there are more than two types of fruits in the window, shrink it
+       
         while (fruitCount.size() > 2) {
             fruitCount[fruits[left]]--; // Reduce the count of the leftmost fruit
             if (fruitCount[fruits[left]] == 0) {
